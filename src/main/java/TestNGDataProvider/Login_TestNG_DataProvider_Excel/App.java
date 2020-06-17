f29607db2ac724dc04809e1053a00a9b2ceafa7f
shoaibhannure <shoaib_hannure@yahoo.com>
2020-06-18 00:26:43 +0530

    Login Test Case using TestNG & Data Provider as Excel Sheet

---
+++ src/main/java/TestNGDataProvider/Login_TestNG_DataProvider_Excel/App.java
@@ -0,0 +1,13 @@
+package TestNGDataProvider.Login_TestNG_DataProvider_Excel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
